7cd77596ec04cca3bb8a5951ac66a72b0384ef41
Budianto IP <budiantoip@gmail.com>
2023-10-07 14:48:20 +0700

    add get all products endpoint

--- packages/faunadb-products-api/src/index.js
+++ packages/faunadb-products-api/src/index.js
@@ -1,9 +1,12 @@
 import {Router, listen} from 'worktop';
+import {getAllProduct} from './products/view.js'
 
 const router = new Router();
 
 router.add('GET', '/', async (request, response) => {
   response.send(200, 'hello world');
 });
+
+router.add('GET', '/products', getAllProduct);
 
 listen(router.run);
