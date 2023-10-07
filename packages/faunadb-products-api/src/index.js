59f2af08f677b264537c88deda407854ef097daa
Budianto IP <budiantoip@gmail.com>
2023-10-07 14:23:25 +0700

    write a simple GET endpoint

--- packages/faunadb-products-api/src/index.js
+++ packages/faunadb-products-api/src/index.js
@@ -1,15 +1,9 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import {Router, listen} from 'worktop';
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+const router = new Router();
+
+router.add('GET', '/', async (request, response) => {
+  response.send(200, 'hello world');
+});
+
+listen(router.run);
